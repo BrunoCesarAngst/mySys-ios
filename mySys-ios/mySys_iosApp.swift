f6ab5855cce4b1cc949b6c3e8fc84ea4cb7b9703
brunocesarangst <bruno.angst@rede.ulbra.br>
2021-05-10 08:45:49 -0300

    Initial Commit

---
+++ mySys-ios/mySys_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mySys_iosApp.swift
+//  mySys-ios
+//
+//  Created by mbcangst on 10/05/21.
+//
+
+import SwiftUI
+
+@main
+struct mySys_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
